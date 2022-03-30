873036b98acd1ce2e3e324e7d9ed6d431b1ae3a1
Reynard Gunawan <reynard.gunawan@student.allenisd.org>
2022-03-30 15:49:53 -0500

    Added left/right mechanics and some extra logs.

--- Sources/ScenesShell/InteractionLayer.swift
+++ Sources/ScenesShell/InteractionLayer.swift
@@ -10,10 +10,10 @@ import Foundation
 class InteractionLayer : Layer, KeyDownHandler {
     let Board = Background()
     let numBlock = block(rect: Rect(size: Size(width: 100, height: 100)))
-    var positions = Array(repeating: Array(repeating: 0, count: 4), count: 4)
+    var positions : [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     var offsetX = 110, offsetY = 110
     
-    func moveCoord(currentMove: String, currentArrow: String, offsetX: inout Int, offsetY: inout Int, positions: inout [[Int]]) {
+    func moveCoord(currentMove: String, currentArrow: String, offsetX: inout Int, offsetY: inout Int, positions: inout [Int]) {
         // Same concept, just apply to multiple blocks. (Only works as intended for 1 block on the board)
         // W Key
         if currentMove == "w" || currentArrow == "ArrowUp"{
@@ -36,29 +36,21 @@ class InteractionLayer : Layer, KeyDownHandler {
         positions[i][j] = value
     }*/
 
-    func generateRandomBlock(positions: [[Int]]) -> Int {
-        var availableSlots : [Int] = []
+    func generateRandomBlock(positions: inout [Int]) {
         // This function is meant to be executed at the end once we have done all calculations on block movement (Board.generateBlock, in InteractiveLayer.swift)
-        for i in 0 ..< 4 {
-            for j in 0 ..< 4 {
-                if positions[i][j] == 0 {
-                    availableSlots.append((i*10)+j)
-                     /* Available Slots meaning
-
-                       First Row: [0, 1, 2, 3]
-                      Second Row: [10, 11, 12, 13]
-                       Third Row: [20, 21, 22, 23]
-                      Fourth Row: [30, 31, 32, 33]
-                     */
-                }
+        let randNum = Int.random(in: 0 ..< 16)
+        let randPercent = Int.random(in: 1 ... 10)
+        if positions[randNum] == 0 {
+            if randPercent == 10 {
+                positions[randNum] = 4
+                checkLose(positions: positions)
+            } else {
+                positions[randNum] = 2
+                checkLose(positions: positions)
             }
+        } else {
+            generateRandomBlock(positions: &positions)
         }
-        let chosenSlot = availableSlots.randomElement()!
-        let x = chosenSlot % 10 // Find last digit of integer
-        let y = Int(chosenSlot / 10) // Find first digit of integer
-        print(x, ", ", y)
-        //insert(entity: numBlock, at: .front)
-        return chosenSlot
     }
     /*func coordToArr(positions: [[Int]], x: Int, y: Int) -> (Int, Int) {
         var xCoord = x, yCoord = y
@@ -68,54 +60,130 @@ class InteractionLayer : Layer, KeyDownHandler {
         return (i, j)
         }*/
 
-    func checkWin() {
-        for i in  0 ..< 4 {
-            for j in 0 ..< 4 {
-                if positions[i][j] == 2048 {
-                    // Add a "You Win" sign here
-                    dispatcher.unregisterKeyDownHandler(handler: self)
-                }
+    func moveRight(positions: inout [Int]) {
+        for i in 0 ..< positions.count {
+            if i % 4 == 0 {
+                let total1 = positions[i]
+                let total2 = positions[i + 1]
+                let total3 = positions[i + 2]
+                let total4 = positions[i + 3]
+                let row = [total1, total2, total3, total4]
+                let filteredRow = row.filter({$0 > 0})
+                let missing = 4 - filteredRow.count
+                let zeros = Array(repeating: 0, count: missing)
+                let newRow = zeros + filteredRow
+                positions[i] = newRow[0]
+                positions[i + 1] = newRow[1]
+                positions[i + 2] = newRow[2]
+                positions[i + 3] = newRow[3]
+                // Doesn't work, troubleshoot filteredRow, etc
+            }
+        }
+    }
+    func moveLeft(positions: inout [Int]) {
+        for i in 0 ..< positions.count {
+            if i % 4 == 0 {
+                let total1 = positions[i]
+                let total2 = positions[i + 1]
+                let total3 = positions[i + 2]
+                let total4 = positions[i + 3]
+                let row = [total1, total2, total3, total4]
+                let filteredRow = row.filter({$0 > 0})
+                let missing = 4 - filteredRow.count
+                let zeros = Array(repeating: 0, count: missing)
+                let newRow = filteredRow + zeros
+                positions[i] = newRow[0]
+                positions[i + 1] = newRow[1]
+                positions[i + 2] = newRow[2]
+                positions[i + 3] = newRow[3]
+            }
+        }
+    }
+    func combineRow(positions: inout [Int]) {
+        for i in 0 ..< 15 {
+            if positions[i] == positions[i + 1] {
+                let combinedTotal = positions[i] + positions[i + 1]
+                positions[i] = combinedTotal
+                positions[i + 1] = 0
+                // Add score here
+                // This works!!!
+            }
+        }
+        checkWin(positions: positions)
+    }
+    
+    func checkWin(positions: [Int]) {
+        for i in  0 ..< 16 {
+            if positions[i] == 2048 {
+                // Add a "You Win" sign here
+                dispatcher.unregisterKeyDownHandler(handler: self)
             }
         }
     }
     
-    func checkLose() {
+    func checkLose(positions: [Int]) {
         var availableSpace = 0
-        for i in 0 ..< 4 {
-            for j in 0 ..< 4 {
-                if positions[i][j] == 0 {
-                    availableSpace += 1
-                }
+        var gameOver = true
+        for i in 0 ..< positions.count {
+            if positions[i] == 0 {
+                availableSpace += 1
             }
         }
-        if availableSpace == 0 {
+        // Combine Row Check
+        for i in 0 ..< 15 {
+            if positions[i] == positions[i + 1] {
+                gameOver = false
+            }
+        }
+        // Combine Column Check
+        for i in 0 ..< 12 {
+            if positions[i] == positions[i + 4] {
+                gameOver = false
+            }
+        }
+        if (gameOver == true) && (availableSpace == 0) {
             // Add a "You Lose" sign here
             dispatcher.unregisterKeyDownHandler(handler: self)
         }
     }
+    func printPos(positions: [Int]) {
+        print("[", positions[0], ",", positions[1], ",", positions[2], ",", positions[3], "]")
+        print("[", positions[4], ",", positions[5], ",", positions[6], ",", positions[7], "]")
+        print("[", positions[8], ",", positions[9], ",", positions[10], ",", positions[11], "]")
+        print("[", positions[12], ",", positions[13], ",", positions[14], ",", positions[15], "]")
+    }
     
     func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool) {
-        print(positions)
         if key == "w" || code == "ArrowUp" {
             print("Detected 'w' key!")
             moveCoord(currentMove: key, currentArrow: code, offsetX: &offsetX, offsetY: &offsetY, positions: &positions)
             numBlock.move(to: Point(x: offsetX, y: offsetY))
-            print(generateRandomBlock(positions: positions))
+            generateRandomBlock(positions: &positions)
+            printPos(positions: positions)
         } else if key == "a" || code == "ArrowLeft" {
             print("Detected 'a' key!")
-            _ = generateRandomBlock(positions: positions)
             moveCoord(currentMove: key, currentArrow: code, offsetX: &offsetX, offsetY: &offsetY, positions: &positions)
             numBlock.move(to: Point(x: offsetX, y: offsetY))
+            generateRandomBlock(positions: &positions)
+            moveLeft(positions: &positions)
+            combineRow(positions: &positions)
+            moveLeft(positions: &positions)
+            printPos(positions: positions)
         } else if key == "s" || code == "ArrowDown" {
             print("Detected 's' key!")
-            _ = generateRandomBlock(positions: positions)
             moveCoord(currentMove: key, currentArrow: code, offsetX: &offsetX, offsetY: &offsetY, positions: &positions)
             numBlock.move(to: Point(x: offsetX, y: offsetY))
+            generateRandomBlock(positions: &positions)
+            printPos(positions: positions)
         } else if key == "d" || code == "ArrowRight" {
             print("Detected 'd' key!")
-            _ = generateRandomBlock(positions: positions)
             moveCoord(currentMove: key, currentArrow: code, offsetX: &offsetX, offsetY: &offsetY, positions: &positions)
             numBlock.move(to: Point(x: offsetX, y: offsetY))
+            moveRight(positions: &positions)
+            combineRow(positions: &positions)
+            moveRight(positions: &positions)
+            generateRandomBlock(positions: &positions)
+            printPos(positions: positions)
         } else {
             print("Detected an unusable key!")
         }
